983dd8762ce4d9d9e25ff79ed4b25fdc06c190d2
rockeyWereWolf(CodeAcademy) <146819852+RockeyWereWolfCodeAcademy@users.noreply.github.com>
2024-07-31 12:07:56 +0400

    Update AuthController.cs
    small auth email fix

--- Twitter.API/Controllers/AuthController.cs
+++ Twitter.API/Controllers/AuthController.cs
@@ -28,36 +28,35 @@ namespace Twitter.API.Controllers
 		[Route("ConfirmEmail")]
         public async Task<IActionResult> ConfirmEmail(string token, string email)
         {
-            var result = await _authService.ConfirmEmail(token, email);
+	    var result = await _authService.ConfirmEmail(token, email);
             var response = result == true ? nameof(ConfirmEmail) : "Unexpected error";
             return new BadRequestObjectResult(response);
         }
 
         //reg
         [HttpPost]
-		[Route("Register")]
-		public async Task<IActionResult> Register(RegisterDTO dto)
+	[Route("Register")]
+	public async Task<IActionResult> Register(RegisterDTO dto)
         {
-            await _userService.CreateAsync(dto);
+           await _userService.CreateAsync(dto);
             
             _emailService.SendEmail(dto.Email, "Welcome", $"<h1>Welcome to our system {dto.UserName}!</h1>" +
-				$"<p>Now you need to confirm your account {await GenerateConfirmationLink(dto)} </p>");
-			return Ok(new { Message = "User Reigstration Successful, please check your email for confirmation link!" });
-		}
+	    $"<p>Now you need to confirm your account {await GenerateConfirmationLink(dto)} </p>");
+	    return Ok(new { Message = "User Reigstration Successful, please check your email for confirmation link!" });
+	}
 
-		//log
-		[HttpPost]
-		[Route("Login")]
-		public async Task<IActionResult> Login(LoginDTO dto)
-		{
-			return Ok(await _authService.Login(dto));
-		}
+	//log
+	[HttpPost]
+	[Route("Login")]
+	public async Task<IActionResult> Login(LoginDTO dto)
+	{
+	    return Ok(await _authService.Login(dto));
+	}
 
         private async Task<string> GenerateConfirmationLink(RegisterDTO dto)
         {
-            var user = _mapper.Map<AppUser>(dto);
-            var token = await _authService.GetConfirmationToken(user);
-            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Auth", new { token, email = user.Email }, Request.Scheme);
+            var token = await _authService.GetConfirmationToken(dto);
+            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Auth", new { token, email = dto.Email }, Request.Scheme);
             return confirmationLink;
         }
     }
